ee85f99ca93a16b05752404624818d02e6516e2a
Rafael <you@example.com>
2023-10-29 09:34:42 -0300

    first commit

---
+++ comandos.js
@@ -0,0 +1,27 @@
+/*
+COMANDOS DE INSTALAÇÃO DO SHADOW BOT - BY: VITUKJK
+
+termux-setup-storage
+
+apt update
+
+apt upgrade
+
+pkg install npm
+
+pkg install nodejs
+
+pkg install python
+
+pkg install
+
+apt install ffmpeg -y
+
+cd /sdcard/Download/SHADOW-BOT-V3
+(se esse não for, tenta o de baixo)
+cd /sdcard/SHADOW-BOT-V3
+
+sh start.sh
+*/
+
+const vaiSeFuder = "Boa sorte"
