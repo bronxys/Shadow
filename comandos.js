32854037b55588b6afbc642a6b27120bb0d4c7f3
Rafael <you@example.com>
2023-11-11 13:00:05 -0300

    att

--- comandos.js
+++ comandos.js
@@ -1,27 +1,22 @@
 /*
-COMANDOS DE INSTALAÇÃO DO SHADOW BOT - BY: VITUKJK
+COMANDOS DE INSTALAÇÃO DO SHADOW-BOT - BY: VITUKJK
 
-termux-setup-storage
-
-apt update
-
-apt upgrade
-
-pkg install npm
-
-pkg install nodejs
-
-pkg install python
-
-pkg install
-
-apt install ffmpeg -y
-
-cd /sdcard/Download/SHADOW-BOT-V3
+1°- termux-setup-storage
+2°- apt update
+3°- apt upgrade
+4°- pkg install npm
+5°- pkg install nodejs
+6°- pkg install python
+7°- pkg install
+8°- apt install ffmpeg -y
+9°- cd /sdcard/Download/SHADOW-BOT-V4
 (se esse não for, tenta o de baixo)
-cd /sdcard/SHADOW-BOT-V3
-
-sh start.sh
+cd /sdcard/SHADOW-BOT-V4
+10°- sh start.sh
 */
 
-const vaiSeFuder = "Boa sorte"
+const vaiSeFuder = "Boa Sorte"
+// Espero q goste do bot. Qualquer dúvida, me chame no zap: +55 21 97198-3758
+// Um beijo, um abraço e um amasso do Vitu e do Gabs <3
+// Ainda não vendo a base descriptografada, mas logo irei começar vender, aguarde. :)
+
