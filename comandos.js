e2cb53188b95c514cb791af5f32f918323485771
Kayser <152158395+vitukjk@users.noreply.github.com>
2024-05-07 16:15:35 -0300

    Add files via upload

---
+++ comandos.js
@@ -0,0 +1,22 @@
+/*
+COMANDOS DE INSTALAÇÃO DO SHADOW-BOT - BY: VITUKJK
+
+1°- termux-setup-storage
+2°- apt update
+3°- apt upgrade
+4°- pkg install npm
+5°- pkg install nodejs
+6°- pkg install python
+7°- pkg install
+8°- apt install ffmpeg -y
+9°- cd /sdcard/Download/SHADOW-BOT-v3.5
+(se esse não for, tenta o de baixo)
+cd /sdcard/SHADOW-BOT-v3.5
+10°- sh start.sh
+*/
+
+const vaiSeFuder = "Boa Sorte"
+// Espero q goste do bot. Qualquer dúvida, me chame no zap: +55 21 97198-3758
+// Um beijo, um abraço e um amasso do Vitu e do Gabs <3
+// Para comprar a base 100% editável, entre em contato com o número acima :)
+
